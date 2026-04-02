0f043934222689c3d71c0389944d0e8f9c5594e6
Moupriya23 <moupriyaroy2001@gmail.com>
2026-04-02 10:43:00 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "30abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "30" => 30
+// "30abc" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = "Moupriya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+// 1 => true, 0 => false
+// "" => false
+// "Mou" => true
+
+let someNumber = true
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
