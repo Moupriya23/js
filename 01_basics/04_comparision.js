89448d97fda0863ae466940f9a4b0ff4053236e6
Moupriya23 <moupriyaroy2001@gmail.com>
2026-04-04 10:53:56 +0530

    comparison check

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+// === -check strictly
+
+console.log("2" == 2);
+console.log("2" === 2);  //it check datatype also
+
+
+// Comparison(<,>,<=,>=, etc) and Equality check(==) both are different
