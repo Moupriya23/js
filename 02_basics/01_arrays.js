f8dd137ec4533ccb92ac04bcb89f5b879cfc26ba
Moupriya23 <moupriyaroy2001@gmail.com>
2026-04-07 18:46:28 +0530

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,22 @@
+// array
+
+const myArr = [1, 2, 3, 4, true, "Mou"]
+const myHeros = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+
+console.log(myArr[5]);
+
+
+// Array Methods
+
+// myArr.push(6)
+// myArr.pop()
+
+myArr.unshift(9)
+
+console.log(myArr);
+
+
+
+
